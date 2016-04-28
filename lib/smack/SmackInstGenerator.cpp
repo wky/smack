972747763d0c06038e6a89639895f78749f8ebc6
Guoanshisb <polarishehn@gmail.com>
2016-04-28 13:53:19 -0600

    Dropped constraint on return pointer of a undefined function

--- lib/smack/SmackInstGenerator.cpp
+++ lib/smack/SmackInstGenerator.cpp
@@ -599,8 +599,8 @@ void SmackInstGenerator::visitCallInst(llvm::CallInst& ci) {
 
   if (f && f->isDeclaration() && rep.isExternal(&ci)) {
     std::string name = naming.get(*f);
-    if (!EXTERNAL_PROC_IGNORE.match(name))
-      emit(Stmt::assume(Expr::fn(Naming::EXTERNAL_ADDR,rep.expr(&ci))));
+    //if (!EXTERNAL_PROC_IGNORE.match(name))
+    //  emit(Stmt::assume(Expr::fn(Naming::EXTERNAL_ADDR,rep.expr(&ci))));
   }
 }
 
