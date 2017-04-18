5db31fa87cc402dbcec8c7517ba4d5f138be38e1
Zvonimir <zvonimir@cs.utah.edu>
2017-04-18 15:01:24 -0600

    Putting relevancy back in

--- share/smack/svcomp/utils.py
+++ share/smack/svcomp/utils.py
@@ -186,6 +186,7 @@ def verify_bpl_svcomp(args):
     if args.bit_precise:
       x = "bopt:" if args.verifier != 'boogie' else ""
       boogie_command += ["/%sproverOpt:OPTIMIZE_FOR_BV=true" % x]
+      boogie_command += ["/%sz3opt:SMT.RELEVANCY=0" % x]
       boogie_command += ["/%sz3opt:SMT.BV.ENABLE_INT2BV=true" % x]
       boogie_command += ["/%sboolControlVC" % x]
 
